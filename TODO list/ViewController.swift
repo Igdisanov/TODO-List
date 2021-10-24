aa97cda16161e6aeb4cabc27bab17557ada7f26b
Igdisanov <igdisanovvr@gmail.com>
2021-10-24 10:15:09 +0500

    Initial Commit

---
+++ TODO list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TODO list
+//
+//  Created by Vadim Igdisanov on 24.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
